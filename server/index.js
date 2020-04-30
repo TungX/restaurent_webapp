9fd54bc831b4baf35272ff934456ff3f81445b72
Hoang Mai <blue.rose.hut@gmail.com>
2020-04-30 22:14:11 +0700

    Fix bug at footer

--- server/index.js
+++ server/index.js
@@ -53,7 +53,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.SERVER_PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
